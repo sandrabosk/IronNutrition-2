2c2719476cc1a0a7cb30cfdb901877ce6270dcbe
sandrabosk <sandraboskovic@hotmail.com>
2017-11-10 23:35:57 -0500

    1st iteration

---
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import foods from '../foods';
+
+@Component({
+  selector: 'app-food-list',
+  templateUrl: './food-list.component.html',
+  styleUrls: ['./food-list.component.css']
+})
+export class FoodListComponent implements OnInit {
+  foods = foods;
+  constructor() { }
+
+  ngOnInit() {
+    this.foods = foods;
+  }
+
+}
